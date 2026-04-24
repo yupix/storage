a1a825ec8c794a0e77c1308276345cec71a901c2
yupix <yupi0982@outlook.jp>
2026-04-24 11:08:14 +0900

    feat: バックエンドの基本的なコードを追加

---
+++ services/api/src/main.rs
@@ -0,0 +1,53 @@
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // トレーサーの初期化
+    tracing_subscriber::fmt::init();
+
+    // ルートを構築する
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+    // hyperを用いてアプリを3000ポートで実行する
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on http://0.0.0.0:3000");
+    axum::serve(listener, app).await;
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // JSONレスポンスに変換する。`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
