961112cfe3f1245e5a598bcc37b96a73112e4619
yupix <yupi0982@outlook.jp>
2026-04-24 14:15:23 +0900

    feat(backend): DB周りの整備

---
+++ services/api/migration/src/lib.rs
@@ -0,0 +1,15 @@
+pub use sea_orm_migration::prelude::*;
+
+mod m20220101_000001_create_table;
+mod m20260424_050952_create_table_users;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            Box::new(m20260424_050952_create_table_users::Migration),
+        ]
+    }
+}
