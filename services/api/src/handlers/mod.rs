60d986c48e61698a9ca7aa4db1e12e620b1819d4
yupix <yupi0982@outlook.jp>
2026-04-28 19:05:50 +0900

    feat(backend): ユーザーテーブルの実装&ユーザーの一覧を返すエンドポイントを実装

---
+++ services/api/src/handlers/mod.rs
@@ -0,0 +1,11 @@
+pub mod users;
+
+#[utoipa::path(
+    get, path = "/",
+    operation_id = "index"
+)]
+pub async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+pub use users::create_user;
