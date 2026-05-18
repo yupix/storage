8bbd7bcab4327b5c22bc65e604cc45135f37e9e6
祥 <sho.halstudent@gmail.com>
2026-05-18 07:42:07 +0000

    build: files.rsを追加

--- apps/api/src/routes/mod.rs
+++ apps/api/src/routes/mod.rs
@@ -3,10 +3,12 @@ use utoipa_axum::router::OpenApiRouter;
 use crate::AppState;
 
 pub mod auth;
-
+pub mod files;
 pub fn create_routes() -> OpenApiRouter<AppState> {
     OpenApiRouter::new().nest(
         "/v1",
-        OpenApiRouter::new().nest("/auth", crate::routes::auth::routes()),
+        OpenApiRouter::new()
+        .nest("/auth", crate::routes::auth::routes())
+        .nest("/files", crate::routes::files::routes()),
     )
 }
