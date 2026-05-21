64987044061173251229bc2531cc331a803fe8f4
default <ohs40018@ohs.hal.ac.jp>
2026-05-21 02:33:16 +0000

    feat:accoun/deleteを作成

---
+++ apps/api/src/handlers/account.rs
@@ -0,0 +1,37 @@
+use axum_session::Session;
+use axum_session_redispool::SessionRedisPool;
+use axum::{Json, extract::State};
+use sea_orm::{EntityTrait};
+use sea_orm::ActiveModelTrait;
+use sea_orm::{ColumnTrait, QueryFilter};
+use crate::entities::users;
+use crate::{AppState, models::user, utils::auth::AuthError};
+
+#[utoipa::path(
+    post,
+    path = "/delete",
+    responses(
+        (status = 200, description = "Delete success", body = String),
+    )
+)]
+
+pub async fn delete(
+    session: Session<SessionRedisPool>,
+    State(state):State<AppState>,
+    
+) -> Result<Json<String>, AuthError> {
+    // sessionからuser_id取得
+    let user_id:i64 = session
+    .get::<i64>("user_id")
+    .ok_or(AuthError::Forbidden)?;
+    // データベースからそのaccountのidを取得 
+    let user= users::Entity::find()
+    .filter(users::Column::Id.eq(user_id))
+    .one(&state.db)
+    .await?
+    .ok_or(AuthError::Forbidden)?;
+    // 削除
+    let active: users::ActiveModel = user.into();
+    active.delete(&state.db).await?;
+    Ok(Json("Delete success".to_string()))
+}
--- apps/api/src/routes/mod.rs
+++ apps/api/src/routes/mod.rs
@@ -1,12 +1,15 @@
 use utoipa_axum::router::OpenApiRouter;
 
-use crate::AppState;
+use crate::{AppState, routes::auth::routes};
+
 
 pub mod auth;
+pub mod account;
 
 pub fn create_routes() -> OpenApiRouter<AppState> {
     OpenApiRouter::new().nest(
         "/v1",
-        OpenApiRouter::new().nest("/auth", crate::routes::auth::routes()),
+        OpenApiRouter::new().nest("/auth", crate::routes::auth::routes())
+        .nest("/account", crate::routes::account::routes()),
     )
 }
