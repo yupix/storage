5a847d31660480ad16911903fa9fdbde1ed7286a
default <jinsan.mail.777@gmail.com>
2026-06-08 07:40:45 +0000

    fix: 読み取ったデータを表示可能に

--- apps/web/src/components/ui/file-link.tsx
+++ apps/web/src/components/ui/file-link.tsx
@@ -2,42 +2,45 @@ import type { ChangeEvent } from "react"
 import { useContext } from "react";
 import { GetFileContext } from "#/contexts/GetFileContext";
 
+const FileLink = () => {
+    let x
+    const {fileinfo,setFile} = useContext(GetFileContext)
 
-const handleFileChange = (e:ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files || e.target.files.length === 0) {
-    return 0;
+    //
+    const jsonTonnel = (file: File): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+
+            reader.onload = () => resolve(reader.result as string)
+            reader.onerror = () => reject(reader.error)
+
+            reader.readAsText(file)
+        })
     }
+
+
+    const handleFileChange = async (e:ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) {
+    return;
+    }
+
     const file = e.target.files[0]
 
-    const reader = new FileReader()
-
-    reader.readAsText(file)
-
-    reader.onload = event => {
-        const content = event.target?.result
-        try {
-            const jsonData = JSON.parse(content as string)
+    try {
+            const content = await jsonTonnel(file)
+            const jsonData = JSON.parse(content)
             console.log(jsonData)
-            console.log(jsonData.files)
-            return jsonData.files
+            console.log(jsonData.files[0])
+            setFile(jsonData.files[0])
             } catch (error) {
             console.error('JSONファイルを解析できませんでした。', error)
-            return 0
         }
     }
-
-    return 0
-}
-
-const FileLink = () =>{
-    let x = 0
-    const {fileinfo,setFile} = useContext(GetFileContext)
     return (
     <div>
         <p>test : {fileinfo}</p>
         <input type="file" accept=".json" onChange={(e) =>{
-            x = handleFileChange(e);
-            setFile(fileinfo+1)
+            handleFileChange(e);
             }}/>
         <div>{x}</div>
     </div>
